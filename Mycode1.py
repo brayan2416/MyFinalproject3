aa680c2e91d75e5dfce1d41d1c6ecdbeb1bb98e9
brayan2416 <55851979+brayan2416@users.noreply.github.com>
2020-04-21 17:22:39 -0400

    In this part for my project i just started coding using the def and selecting how to shoe the figure the next part for me will be how to create the figure and add to my code i'll be using while for that. i am going to work on that tonight.

---
+++ Mycode1.py
@@ -0,0 +1,17 @@
+from NBAnames import NBAnames.py
+import random
+
+def pick():
+    NBAnames = random.choice(NBAnames.py)
+    return word.upper()
+
+def choice(word):
+    word_completion = +.+ * len(word)
+    correctwords = false = []
+    correctwords_letters = []
+    attempts = 5
+
+    print("are you ready to play?")
+    print(show_figure(attempts))
+    print(word_completion)
+    print("\n")
