5cc929bef9aceb342088377403d587ba41ae4434
brayan2416 <55851979+brayan2416@users.noreply.github.com>
2020-04-29 16:57:33 -0400

    I have started finishing my project i added the if and elif i still have some project trying to run the code maybe i need to fix couple of things I'll work on all this during the week and if i need help i'll contact you before friday.

--- Mycode1.py
+++ Mycode1.py
@@ -1,17 +1,38 @@
-from NBAnames import NBAnames.py
+from nbanames import name_list
 import random
 
-def pick():
-    NBAnames = random.choice(NBAnames.py)
-    return word.upper()
+def get_nbanames():
+    nbanames = random.choice(name_list)
+    return nbanames.upper()
 
-def choice(word):
-    word_completion = +.+ * len(word)
+def game(nbanames):
+    nbanames_completion = +-+ * len(nbanames)
     correctwords = false = []
     correctwords_letters = []
     attempts = 5
-
     print("are you ready to play?")
     print(show_figure(attempts))
-    print(word_completion)
+    print(nbanames_completion)
     print("\n")
+
+    while not corectwords and attemps > 0:
+     correctwords = input("I want you to try a letter or a word:").upper()
+     if len(correctwords) == 1 and correctwords.isalpha():
+        if correctwords in correctwords_letters():
+           print("you get your first word correct", correctwords)
+        elif correctwords not in nbanames:
+            print(correctwords, "This is not the right word.")
+            attempts -= 1
+            correctwords_letters.append(nbanames)
+        else:
+            print("Very well guessed,",correct, "is in the word.")
+            correctwords_letters.append(correct)
+            nbanames_as_list = list(nbanames_completion)
+
+      elif len(correctwords) == len(nbanames) and correctwords.isalpha():
+
+        else:
+            Print("This is not a correct guess")
+        print(show_figure(attemps))
+        print(nbanames_completion)
+        prin("\n")
